a287d8611d6f86237a18efa166f5e26adb58adb2
M0Bss <bemjamintrigo42@gmail.com>
2023-05-26 00:23:52 -0400

    Modelo

--- bodegaCentralMP/bodega/models.py
+++ bodegaCentralMP/bodega/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+#run-nombre-apellido-correo-contraseña
+class Usuario(models.Model):
+    run=models.CharField(max_length=15)
+    nombre=models.CharField(max_length=30)
+    apellido=models.CharField(max_length=30)
+    email=models.EmailField()
+    password=models.CharField(max_length=20)
