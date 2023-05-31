96094aab3e54437c088e946f6a66144ec51fa73d
Vixencius1 <vi.donoso@duocuc.cl>
2023-05-31 17:16:37 -0400

    Avanzando en el formulario, url y demás

--- bodegaCentralMP/bodega/forms.py
+++ bodegaCentralMP/bodega/forms.py
@@ -4,36 +4,6 @@ from .models import *
 
 # Ingresar aquí las clases de la base de datos
 
-# Ejemplo:
-#
-# class MiembroForm(ModelForm):
-#     class Meta:
-#         model = Miembro
-#         fields = [
-#             'idMiembro',
-#             'nombre',
-#             'apellidos',
-#             'ocupacion',
-#             'correo',
-#             'foto'
-#         ]
-#         labels = {
-#             'idMiembro': 'ID Miembro',
-#             'nombre': 'Nombre',
-#             'apellidos': 'Apellidos',
-#             'ocupacion': 'Ocupación',
-#             'correo': 'Correo',
-#             'foto': 'Foto'
-#         }
-#         widgets = {
-#             'idMiembro': forms.TextInput(attrs={'class':'form-control','type':'number'}),
-#             'nombre': forms.TextInput(attrs={'class':'form-control'}),
-#             'apellidos': forms.TextInput(attrs={'class':'form-control'}),
-#             'ocupacion': forms.TextInput(attrs={'class':'form-control'}),
-#             'correo': forms.TextInput(attrs={'class':'form-control'}),
-#             'foto': forms.FileInput(attrs={'class':'form-control'})
-#         }
-
 class UsuarioForm(ModelForm):
     class Meta:
         model = Usuario
