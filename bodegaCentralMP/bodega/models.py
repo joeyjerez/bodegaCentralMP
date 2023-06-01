05754748014aac5fdf37ef09a06371eaa2289ea7
Vixencius1 <vi.donoso@duocuc.cl>
2023-05-31 23:18:34 -0400

    Pequeños detalles

--- bodegaCentralMP/bodega/models.py
+++ bodegaCentralMP/bodega/models.py
@@ -1,25 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
-# Ejemplo
-#
-# class Miembro(models.Model):
-#     idMiembro = models.IntegerField(primary_key=True, verbose_name='ID')
-#     nombre = models.CharField(max_length=20, verbose_name='Nombre')
-#     apellidos = models.CharField(max_length=30, verbose_name='Apellidos')
-#     ocupacion = models.CharField(max_length=100, verbose_name='Ocupación')
-#     correo = models.CharField(max_length=50, verbose_name='Correo')
-#     foto = models.ImageField(verbose_name='Foto', upload_to='nosotros',null=False)
-
-#     class Meta:
-#         verbose_name='miembro'
-#         verbose_name_plural='miembros'
-#         ordering=['idMiembro']
-
-#     def __str__(self):
-#         return self.nombre
-
 class Usuario(models.Model):
     rut = models.CharField(max_length=21, verbose_name='RUT', null=False, default='0-0')
     nombre = models.CharField(max_length=40, verbose_name='Nombre', null=False)
