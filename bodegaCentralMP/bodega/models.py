afe4b13bfaced0fff90262887670b9a6c9a8ff54
M0Bss <bemjamintrigo42@gmail.com>
2023-05-31 23:57:34 -0400

    Permisos de usuarios
    Relacionado con superuser status, mostrando tablas limitadas para no superusuarios

--- bodegaCentralMP/bodega/models.py
+++ bodegaCentralMP/bodega/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 
-class Usuario(models.Model):
-    run = models.CharField(max_length=12, unique=True)
-    nombre = models.CharField(max_length=100)
-    apellido = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    password=models.CharField(max_length=20)
+class Producto(models.Model):
+    codigo = models.IntegerField(verbose_name='Codigo', null=False)
+    nombre = models.CharField(max_length=50, verbose_name='Nombre', null=False)
+    stock = models.IntegerField(verbose_name='Stock', null=False, default=0)
+    descripcion = models.CharField(max_length=400, verbose_name='Descripción')
+    marca = models.CharField(max_length=80, verbose_name='Marca')
+    precio = models.IntegerField(verbose_name='Precio', null=False, default=0)
+    imagen = models.ImageField(null=True, blank=True, verbose_name='Imagen', upload_to='productos/')
+    
 
     class Meta:
-        verbose_name = "usuario"
-        verbose_name_plural = "usuarios"
-        ordering = ["email"]
+        verbose_name='producto'
+        verbose_name_plural='productos'
+        ordering=['codigo', 'marca']
+
+    def __str__(self):
+        return self.nombre
